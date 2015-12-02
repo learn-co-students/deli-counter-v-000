2e2df9594605405371af739fafc03343f9040f5e
Dakota Martinez <dakotaleemusic@gmail.com>
2015-12-02 23:50:52 +0000

    completes deli counter lab

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+katz_deli = []
+
+def line(queue)
+   if queue.count != 0
+      output = ""
+       queue.each_with_index do |person, index|
+           output += " #{index+1}. #{person}"
+       end
+       puts "The line is currently:#{output}"
+   else
+       puts "The line is currently empty." 
+   end
+end
+
+def take_a_number(line, person)
+    line << person
+    puts "Welcome, #{person}. You are number #{line.count} in line."
+end
+
+def now_serving(line)
+    if line.count == 0
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{line.shift}."
+    end
+end
