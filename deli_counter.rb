255a800cc8501b83b640f8c27e1a689d9f17eae2
Shmuwol <shmully.wolfson@gmail.com>
2015-12-09 21:56:51 -0500

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,4 +15,10 @@ def take_a_number(array, name)
   puts "Welcome, #{name}. You are number #{array.index(name) +1} in line."
 end
 
-
+def now_serving(array)
+	if array.empty? 
+		puts "There is nobody waiting to be served!"
+	else
+		puts "Currently serving #{array.shift}."
+  end
+end
