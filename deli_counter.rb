9a889611ab015ba5ebb7121094dba33074106edb
macgeek30 <mike25ko@gmail.com>
2015-12-16 00:46:09 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(katz_deli)
+  line = " "
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else
+    katz_deli.each_with_index do |cust, i|
+      line << " #{i+1}. #{cust}"
+    end
+    puts "The line is currently:#{line}"
+  end
+end
