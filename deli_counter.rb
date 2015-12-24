336a89bf2e9773fd13830411a1a1aac0f54684dc
Richard Shen <rich.shen@nyu.edu>
2015-12-24 22:03:14 +0000

    Added deli_counter.rb

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+ katz_deli = []
+
+# Write your code here
+def line(array)
+  phrase = "The line is currently:"
+  if array==[]
+    puts "The line is currently empty."
+  else
+    array.each do |person|
+      phrase+=" #{array.index(person)+1}. #{person}"
+    end
+    puts phrase
+  end
+end
+
+def take_a_number(array, name)
+  array << name
+  puts "Welcome, #{name}. You are number #{array.size} in line."
+end
+
+def now_serving(array)
+  if array == []
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array.first}."
+    array.shift
+  end
+end
