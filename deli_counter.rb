eb1007395073445d0286e64966a2b45f3ede0d58
Chris Bruen <bruen.chris@gmail.com>
2015-12-26 11:06:39 -0500

    fix

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,10 +2,12 @@
 
 def line(katz)
 	if katz.size == 0
-		puts "The line is currently empty"
+		puts "The line is currently empty."
 	else
-		puts "The line is currently: "
-		katz.each {|x| puts "#{katz.index(x)+1}. #{x}"}
+		output = []
+		katz.each { |x| output.push("#{katz.index(x)+1}. #{x}") }
+		output = output.join(" ")
+		puts "The line is currently: " + output
 	end
 end
 
@@ -13,4 +15,13 @@ end
 def take_a_number(katz,person)
 	katz << person
 	puts "Welcome, #{person}. You are number #{katz.index(person)+1} in line."
+end
+
+def now_serving(katz)
+	unless katz.size == 0
+		puts "Currently serving #{katz[0]}."
+		katz.shift()
+	else
+		puts "There is nobody waiting to be served!"
+	end
 end
