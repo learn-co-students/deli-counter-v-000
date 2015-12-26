705da6c657d3b8b9cbe01953c1e3651b55a31c7a
Chris Bruen <bruen.chris@gmail.com>
2015-12-26 10:40:04 -0500

    added methods

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+
+def line(katz)
+	if katz.size == 0
+		puts "The line is currently empty"
+	else
+		puts "The line is currently: "
+		katz.each {|x| puts "#{katz.index(x)+1}. #{x}"}
+	end
+end
+
+
+def take_a_number(katz,person)
+	katz << person
+	puts "Welcome, #{person}. You are number #{katz.index(person)+1} in line."
+end
