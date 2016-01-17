1a10fd680d5fbf422f6e762839a8d6d8e1525988
aenser <aaronenser1@gmail.com>
2016-01-17 14:07:09 -0700

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,28 +1,28 @@
-def line(katz_deli)
-  if katz_deli.size > 0
-    line_phrase = "The line is currently:"
-    katz_deli.each_with_index { |name, pos|
+def line(array)
+  if array.size > 0
+    phrase = "The line is currently:"
+    array.each_with_index do |name, pos|
       pos += 1
-      line_phrase << " #{pos}. #{name}"
-    }
-    puts line_phrase
-  else
-    puts "The line is currently empty."
-end
+      phrase << " #{pos}. #{name}"
+    end
+    puts phrase
+  else 
+    puts "The line is currently empty." 
+    end
 end
 
 def take_a_number(katz_deli, name)
-  katz_deli.push(name)
+ katz_deli << "#{name}"
 
-  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+ puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
-
 
 def now_serving(katz_deli)
   if katz_deli.size > 0
     puts "Currently serving #{katz_deli.first}."
     katz_deli.shift
   else
-    puts "There is nobody waiting to be served!"
-end
-end
+   puts "There is nobody waiting to be served!"
+ end
+
+end 
