1ab145247715a57a2b64d8695ddc04a7ce2ca681
Ann Lee <aplee29@users.noreply.github.com>
2016-02-01 01:26:07 -0500

    define methods to keep track of deli customers

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def line(customers)
+  if customers.size == 0
+    puts "The line is currently empty."
+  else
+    line = "The line is currently:"
+    customers.each_with_index do |customer, index|
+      line << " #{index + 1}. #{customer}"
+    end
+    puts line
+  end
+end
+
+def take_a_number(customers, name)
+  customers << name
+  puts "Welcome, #{name}. You are number #{customers.count} in line."
+end
+
+def now_serving(customers)
+  if customers.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{customers[0]}."
+    customers.shift
+  end
+end
