397707ee7eb341a92a5513f8e9707f4003532a4b
bedwardsphoto <brian@brianedwardsphotography.com>
2016-02-10 18:57:17 -0500

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,33 @@
-# Write your code here.
+def line(array)
+  count=array.count
+if count==0
+  puts "The line is currently empty."
+else
+  x=0
+  num=1
+  response="The line is currently:" 
+   array.each do |name|
+   response+=" #{num}. #{name}" 
+    x+=1
+    num+=1
+   end
+   puts response
+  end
+end
+
+def take_a_number(array,name)
+  katz_deli=array
+  katz_deli<<name
+  position=katz_deli.count
+  puts "Welcome, #{name}. You are number #{position} in line."
+  return katz_deli
+end
+
+def now_serving(array)
+  if array.count==0
+    puts "There is nobody waiting to be served!"
+  else
+    puts"Currently serving #{array[0]}."
+    array.delete_at(0)
+  end
+end
