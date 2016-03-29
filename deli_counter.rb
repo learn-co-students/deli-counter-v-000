6b1e67aa03a495cbcd826fa7d32e549b03a7b2c1
Certain125 <amanita.certain@gmail.com>
2016-03-29 15:25:39 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,3 +13,26 @@ def line(katz_deli)
   puts "The line is currently:#{line}"
   end
 end
+
+def take_a_number(katz_deli, new_customer)
+  number_in_line = katz_deli.size + 1
+  puts "Welcome, #{new_customer}. You are number #{number_in_line} in line."
+  katz_deli << new_customer
+end
+
+def now_serving(katz_deli)
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
+  end
+end
+
+
+#check for minor errors like misspellings and punctuation / spaces 
+#check for order of statements!!!!!
+#check for indentations/alignment!!!!!!
+#check to see if you can build a string as w/ line(katz_deli)
+#make sure all the ends are in order!!!!!!!
+#check your arguments!!!!!!
