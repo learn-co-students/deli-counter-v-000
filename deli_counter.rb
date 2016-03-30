add41635bc413e03d9e1f4c766b866f029aa4023
kade042 <dereje88@gmail.com>
2016-03-30 23:31:45 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+#require 'pry'
+def line(katz_deli)
+
+   #binding.pry
+  if katz_deli.first == nil
+    puts "The line is currently empty."
+  else
+    array = Array.new
+    array << "The line is currently:"
+    katz_deli.each_with_index { |name, line| array << " #{line+1}. #{name}" }
+    puts array.join("")
+
+  end
+
+end
+
+def take_a_number(katz_deli, name)
+   katz_deli << name
+   puts "Welcome, #{name}. You are number #{katz_deli.rindex(name)+1} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.first == nil
+    puts "There is nobody waiting to be served!"
+
+  else
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
+  end
+end
