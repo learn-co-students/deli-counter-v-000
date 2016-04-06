772db1adfa3fc9b079f5d9ff6723013fe11d792e
MIKE-RANDELL <michael.james.randell@gmail.com>
2016-04-06 16:06:44 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,3 +15,19 @@ def line(katz_deli)
 
   end
 end
+
+def take_a_number(katz_deli, name)
+  linenum = katz_deli.length + 1
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{linenum} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.length < 1
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.delete_at(0)
+  end
+end
+
