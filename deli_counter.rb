3977f76552d9f3023e5fcc5839f08eeb548bd47b
MIKE-RANDELL <michael.james.randell@gmail.com>
2016-04-06 15:24:34 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.length < 1
+    puts "The line is currently empty."
+  else
+    katzarr = []
+    katz_deli.each_with_index do |name, number|
+      katzarr << "#{number+1}. #{name}"
+    end
+    string = katzarr.join(" ")
+    puts "The line is currently: " + string
+
+  end
+end
