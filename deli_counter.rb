bdb033dedf99369ad3b47889e3ed256c69ef92ff
Jonathan Stone <jon_p_stone@yahoo.com>
2016-04-20 01:35:01 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,26 +1,18 @@
 def line(deli)
-  queue = "The line is currently: "
+    queue = "The line is currently:"
   if deli.size == 0
     puts "The line is currently empty."
   else
     deli.each_with_index do | name, index |
-      queue << index.to_str + 1
-      queue << name
+      queue << " #{index + 1}. #{name}"
     end
     puts queue
   end
 end
 
 def take_a_number(deli, name)
-  if deli.size == 0
   deli << name
-    puts "Welcome, #{name}. You are number 1 in line."
-  else
-    deli << name
-    deli.each_with_index do | customer, index |
-      puts "Welcome, #{customer}. You are number #{index + 1} in line."
-    end
-  end
+  puts "Welcome, #{name}. You are number #{deli.size} in line."
 end
 
 def now_serving(deli)
