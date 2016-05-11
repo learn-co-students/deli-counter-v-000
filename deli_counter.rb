fc714b7cc883ed76c6af33f5f4622dcbab1ae75b
Anita Ford <anitamford@gmail.com>
2016-05-10 21:59:03 -0500

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+def line(katz_deli)
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else
+    print "The line is currently:"
+    count = 0
+    while count < katz_deli.size
+      print " #{count + 1}. "
+      print katz_deli[count]
+      count += 1
+    end
+    print "\n"
+  end
+end
+
+
+def take_a_number(katz_deli)
+
+end
+
+
+def now_serving(katz_deli)
+
+end
