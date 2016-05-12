b4a0541ee53d0a95e23d355ec72d2e4cb9c373c7
Emmanuel Owusu <e_owusu@live.com>
2016-05-11 22:53:18 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,58 @@
 # Write your code here.
+#queue is represented by an empty array
+katz_deli = []
+
+
+#line method, shows customers their places in line
+def line(katz_deli)
+	line_count = 1
+  wordz = "The line is currently:"
+
+  if katz_deli.length == 0
+    wordz = "The line is currently empty."
+  else
+    katz_deli.each do |person|
+    	wordz << " #{line_count}. #{person}"
+        line_count += 1
+    end
+    end
+    puts wordz
+end
+
+
+
+
+
+def take_a_number(katz_deli, new_person)
+  wordz = "Welcome, "
+  line_count = 1
+	position = 0
+	katz_deli << new_person
+
+	if katz_deli.length == 0
+    wordz = "Welcome, #{new_person}. You are number #{line_count} in line."
+  else
+		katz_deli.each do |customer|
+    wordz << "#{new_person}. You are number #{line_count[position]} in line."
+    line_count += 1
+		position += 1
+  end
+end
+  puts wordz
+end
+
+
+
+def now_serving(katz_deli)
+	wordz = "Currently "
+	if katz_deli.length == 0
+		wordz = "There is nobody waiting to be served!"
+	end
+
+	katz_deli.each do |person|
+		katz_deli.shift
+		wordz << "serving #{person}."
+
+	end
+	puts wordz
+end
