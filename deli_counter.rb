f9c6b041e3c09dec9d164c79d8d5ccb47f3aa6f3
Caleb Faught <faughtcj@gmail.com>
2016-05-27 16:28:55 -0500

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(deli_line)
+  if deli_line.size == 0
+    puts "the line is currently empty."
+  else
+    deli_line.each_with_index do |place, index|
+      puts "#{place}, #{index + 1}"
+    end
+  end
+end
+
+def take_a_number(katz_deli, name)
+
+end
