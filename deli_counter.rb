6765ded546ec81f1f92f92e91010eb6b7f68248c
scottibe <bewick.scott@gmail.com>
2016-06-03 17:29:57 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+require 'pry'
+katz_deli = []
+def line(deli)
+  if deli == []
+  puts "The line is currently empty."
+  else
+  the_line = "The line is currently:"
+  deli.each.with_index(+ 1) do |people, index|
+  the_line << " #{index}. #{people}"
+  end
+  puts the_line
+  end 
+end
+
+
+def take_a_number(katz_deli, "Ada")
+  deli = Array.new(1, ada)
+  puts deli
+  katz_deli.each.with_index(1) do |name, i|
+  puts "Welcome, #{name}. You are number #{i} in line." 
+  end
+end
