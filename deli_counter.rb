48234a3c74c2d587bec78d07e741657517b43446
Perla Yates <pianistps@yahoo.com>
2016-06-08 04:48:18 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     sentence  = "The line is currently: "
-    katz_deli.each_with_index {|name, index| sentence << "#{index + 1}. #{name.chomp} " }
+    katz_deli.each_with_index {|name, index| sentence << "#{index + 1}. #{name} " }
     puts sentence
   end
 end
