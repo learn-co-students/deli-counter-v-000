06399979219b08f55ccb919fa33f58a8658753be
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-06-10 15:40:32 +0000

    After passing RSPec tests

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def line(deli)
+    if deli.empty?
+        puts "The line is currently empty."
+    else
+        the_string = "The line is currently:"
+        deli.each_with_index do |name, index| 
+           the_string += " #{index+1}."
+           the_string += " #{name}"
+        end
+        puts the_string
+    end
+end
+
+def take_a_number(deli, name)
+    deli << name
+    the_string = "Welcome, #{name}. You are number #{deli.length} in line."
+    puts the_string
+end
+
+def now_serving(deli)
+    if deli.empty?
+        puts "There is nobody waiting to be served!"
+    else
+       puts "Currently serving #{deli.shift}." 
+    end
+end
