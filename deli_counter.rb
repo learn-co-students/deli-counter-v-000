8fc901ceecf9765ddb63b4bc400e95a4377c9ba5
Jesse Novotny <jessenovotny@gmail.com>
2016-06-14 16:59:03 -0700

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+katz_deli = []
+
+
+
+def line(katz_deli)
+  if katz_deli.size == 0
+    puts "The line is currently empty."
+  else
+    puts "The line is currently: 1. #{katz_deli[0]} 2. #{katz_deli[1]} 3. #{katz_deli[2]}"
+  end
+end
+
+def take_a_number(katz_deli, name)
+  if katz_deli.size == 0
+    puts "Welcome, #{name}. You are number 1 in line."
+    katz_deli.push(name)
+  else
+    puts "Welcome, #{name}. You are number 4 in line."
+    katz_deli.push(name)
+  end
+end
+
+def now_serving
+
+end
+
+
