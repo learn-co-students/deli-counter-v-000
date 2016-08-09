2f41d6f50b061e8560878462d5e87ce406a4397a
Benjamin Cantlupe <bjcantlupe@gmail.com>
2016-08-09 03:06:12 +0000

    deli counter methods

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+def line(line)
+  if line.empty?
+    puts "The line is currently empty."
+  else
+    counter = 1
+    string = "The line is currently:"
+    line.each do |name|
+      string << " #{counter}. #{name}"
+      counter += 1
+    end
+    puts string
+  end
+end
+
+def take_a_number(line, name)
+  line << name
+  num = line.length
+  puts "Welcome, #{name}. You are number #{num} in line."
+end
+
+def now_serving(line)
+  if line.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{line[0]}."
+    line = line.shift
+    return line
+  end
+end
