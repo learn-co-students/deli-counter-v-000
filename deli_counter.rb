a11e19d48cb082f99ccfd2b86669f2d313fd4005
Jason Casares <jasonc@csdrafting.com>
2016-08-15 14:55:10 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,19 +1,26 @@
 # Write your code here.
-def line(katz_deli)
-  katz_deli = []
-  if katz_deli.empty?
+def line(deli)
+  if deli.empty?
     puts "The line is currently empty."
   else
-    katz_deli.each_with_index do |name, index|
-    "The line is currently: #{index}. #{name}"
+    current_line = "The line is currently:"
+    deli.each.with_index(1) do |person, i|
+      current_line << " #{i}. #{person}"
     end
+    puts current_line
   end
 end
 
-#def take_number(katz_deli, name)
+def take_a_number(deli, name)
+  deli << name
+  puts "Welcome, #{name}. You are number #{deli.length} in line."
+end
 
-#end
-
-#def now_serving
-  #puts "There is nobody waiting to be served!"
-#end
+def now_serving(deli)
+  if deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{deli.first}."
+    deli.shift
+  end
+end
