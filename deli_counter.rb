99f095159b123c0b761dfa2afc564109ddfc1439
Tanner Lancaster <saltillokid11@yahoo.com>
2016-08-30 13:41:18 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+
+def line(katz_deli)
+  if currentPlace.empty?
+    puts "The line is currently empty."
+  else
+    display_line = "The line is currently:"
+    
+  end
+end
+
+def take_a_number(katz_deli, name)
+  puts "hi"
+end
+
+def now_serving
+  if line > 0
+    puts "Welcome, #{name}. You are number #{currentPlace} in line."
+  else
+    puts "There is nobody waiting to be served!"
+  end
+end
