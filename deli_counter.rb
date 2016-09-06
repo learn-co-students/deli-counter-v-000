99bba4e41ea12212e8f19c8e2802821ea1e31cee
noson cohen <natancohen23@gmail.com>
2016-09-06 17:58:50 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+katz_deli = ["Logan", "Avi", "Spencer"]
+
+def line(katz_deli)
+  katz_deli.each_with_index do |name, index|
+    puts "The line is currently: #{index + 1}. #{name}."
+  #if deli == []
+    #puts "The line is currently empty."
+  end
+end
