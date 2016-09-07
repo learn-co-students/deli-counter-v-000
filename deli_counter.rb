32361dce7c93035349316072b01c977113c5af01
noson cohen <natancohen23@gmail.com>
2016-09-07 18:48:26 +0000

    built #line #take_a_numer #now_serving

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,28 @@
-katz_deli = ["Logan", "Avi", "Spencer"]
+katz_deli = []
 
 def line(katz_deli)
-  katz_deli.each_with_index do |name, index|
-    puts "The line is currently: #{index + 1}. #{name}."
-  #if deli == []
-    #puts "The line is currently empty."
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else
+    string = "The line is currently:"
+   katz_deli.each_with_index do |name, index|
+     string << " #{index + 1}. #{name}"
+  end
+   puts string
   end
 end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+  end
+
+
+def now_serving(katz_deli)
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.first}."
+    end
+    katz_deli.shift
+  end
