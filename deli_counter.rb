97e66f4ccec12a45c51d2063bcd664730b687858
Jingru Zhang <zhang.jingru.93@gmail.com>
2017-02-24 23:48:01 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@
 katz_deli = []
 
 def line(katz_deli)
-  if katz_deli == []
+  if katz_deli.empty?
     puts "The line is currently empty."
   else
     array = []
@@ -15,11 +15,11 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  puts "Welcome, #{name}. You are number #{katz_deli.index(name)+1} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving(katz_deli)
-  if katz_deli == []
+  if katz_deli.empty?
     puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{katz_deli.shift}."
