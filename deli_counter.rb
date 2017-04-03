707b592455e052f51b2b8cbcf2aea9f54027dc8d
Michael Ries <michael.ries@gmail.com>
2017-04-03 16:34:30 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+def line(queue)
+  if queue.size==0
+    puts "The line is currently empty." 
+  else
+    result="The line is currently:"
+    # for i in 1..queue.size
+    #   result += " #{i}. #{queue[i-1]}"
+    # end
+    queue.each do |customer|
+      result += " #{queue.index(customer)+1}. #{customer}"
+    end
+    puts result
+  end
+end
+
+def take_a_number(queue, name)
+  queue<<name
+  puts "Welcome, #{name}. You are number #{queue.size} in line."
+end
+
+def now_serving(queue)
+  if queue.size==0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{queue.shift}."
+  end
+end
