639e9e3db7539ac43de256c0e6426aa32951a646
Zain <zain@halai.us>
2017-04-06 14:04:02 -0500

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,11 @@
 def line(katz_deli)
-	if katz_deli.length > 0
+	if !katz_deli.empty?
 		current_line = "The line is currently:"
 
 		katz_deli.each_with_index do |customer, index|
 			current_line += " #{index + 1}. #{customer}"
 		end
-		
+
 		puts current_line
 	else
 		puts "The line is currently empty."
@@ -18,7 +18,7 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-	if katz_deli.length > 0
+	if !katz_deli.empty?
 		puts "Currently serving #{katz_deli.shift}."
 	else
 		puts "There is nobody waiting to be served!"
