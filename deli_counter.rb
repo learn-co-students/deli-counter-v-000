95a18d2f069cf6f556188b80d54d7de8b5cba236
Joseph Hyatt <jhyatt1017@gmail.com>
2017-04-12 20:52:29 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,21 @@ def line(katz_deli)
     line_list = "The line is currently:"
     katz_deli.each_with_index do |person, index|
       line_list << " #{index + 1}. #{person}"
-    end 
+    end
     puts line_list
+  end
+end
+
+def take_a_number(katz_deli, person)
+  katz_deli << person
+  puts "Welcome, #{person}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift
   end
 end
