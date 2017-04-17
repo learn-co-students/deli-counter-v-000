587f6581b195ec1fca061009f1b430b98ce2ef0b
Deborah Johnson <debjohnson_11@yahoo.com>
2017-04-17 20:06:44 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,13 +2,16 @@
 katz_deli = []
 
 def line(katz_deli)
+  current_serve = "The line is currently:"
+  period = "."
+  new_line = []
   if katz_deli == []
     puts "The line is currently empty."
   else
     katz_deli.each_with_index do |customer, index|
-        
+        new_line << "#{index + 1}#{period}#{customer}"
     end
-    puts "The line is currently: #{katz_deli[index]}"
+    puts "The line is currently: #{new_line}"
   end
 
 end
