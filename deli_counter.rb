1013d52e1c09719c6e902a79a836b8724170fb01
Deborah Johnson <debjohnson_11@yahoo.com>
2017-04-17 18:42:45 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli == []
+    puts "The line is currently empty."
+  else
+    katz_deli.each_with_index do |customer, index|
+        
+    end
+    puts "The line is currently: #{katz_deli[index]}"
+  end
+
+end
