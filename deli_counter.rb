4fad0ae01a50b482450b53d815f9c4f59d0e3b63
Yujun Sung <yujunsung@gmail.com>
2017-05-02 17:43:27 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+
+def line(katz_deli)
+  count = 0
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else
+     kats_deli.each do |order|
+      puts "#{count}. #{order}"  
+      count += 1
+    end
+  end
+end 
