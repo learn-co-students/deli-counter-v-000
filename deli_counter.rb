2a56652e7137f3b1cc93672816f5641258d8780c
Seb Gertz <sebgertz@gmail.com>
2017-06-08 18:41:15 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+
+katz_deli = []
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  "Welcome #{name}. You are number #{katz_deli.index(name)+1} in line."
+end
+
+def line(katz_deli)
+  numbers_names_array = []
+  if katz_deli == []
+    "The line is currently empty"
+  else
+    katz_deli.each do |name|
+      numbers_names_array << "#{katz_deli.index(name)+1}. #{name}"
+    end
+    "The line is currently: " + numbers_names_array
+  end
+end
