aba96bf74ec16e290801968c00980d1942bef611
Joshua Vickstrom <joshuavickstrom@outlook.com>
2017-06-12 19:39:20 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+
+def line(katz_deli)
+    if katz_deli == []
+    puts "The line is currently empty."
+    else
+    katz_deli.each_with_index {|name, place| katz_deli << "The line is currently: #{place +1} #{name}!"}
+    end
+    katz_deli
+end
+
+def take_a_number(katz_deli, name)
+
+end
+
+def now_serving()
+end
