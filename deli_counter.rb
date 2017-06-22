08306f348501135e043c3b3b8bc9604e747066d5
Erica Plofsky <ericaplofsky@gmail.com>
2017-06-22 03:27:27 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  katz_deli.each do |customer|
+  if katz_deli == " "
+    put "The line is currently empty."
+  else
+    put "The line is currently #{customer}."
+  end
+end
+end
