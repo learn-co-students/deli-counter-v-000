0f393af89216f414d76b4eb49babc991cfa4311e
Karen Berba <kberba@gmail.com>
2017-06-29 03:14:43 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,21 +5,18 @@ katz_deli = []
 
 # note: when using 'each', remember to place it in variable in order to save that value.. 'each' doesn't save the value
 def line( katz_deli )
-  line_array = []
-  katz_deli.each_with_index do |name, index|
-    line_array << "#{index + 1}. #{name}"
-  end
-  while (line_array.length > 0)
-    newline = line_array.each do |person|
-      puts person
-    end
-  end
-  #newline = line_array.map {|person| puts person}
   if katz_deli != []
-    return ("The line is currently: " +  newline)
+    line_array = []
+    katz_deli.each_with_index do |name, index|
+      line_array << " #{index + 1}. #{name}"
+    end
+    newline = ""
+    line_array.each do |person|
+      newline << person
+    end
+    puts "The line is currently:" + "#{newline}"
   else
     puts "The line is currently empty."
-    # put something here
   end
 end
 
