c09a74702b9ce0a7fac3a01e5ced1ee10c00cd2a
Sydney Chun <schun08@gmail.com>
2017-07-02 22:31:54 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,28 +1,59 @@
-#puts the current status of the line
+# first attempt (6/13/17)
+# puts the current status of the line
+# can potentially refactor this a bit with a conditional for what comes after "The line is currently"
+# def line(katz_deli)
+#   if katz_deli.length == 0
+#     puts "The line is currently empty."
+#   else
+#     deli_line = "The line is currently: "
+#     katz_deli.each_with_index do |person, index|
+#       deli_line += "#{index+1}. #{person} "
+#     end
+#     puts deli_line.strip! #this removes any trailing whitespace from the string
+#   end
+# end
+#
+# #add a person to the end of the line
+# def take_a_number(katz_deli, name)
+#   katz_deli.push(name)
+#   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+# end
+#
+# #tells you who is currently being served
+# #if there a more elegant way to do this too?
+# def now_serving(katz_deli)
+#   if katz_deli.length == 0
+#     puts "There is nobody waiting to be served!"
+#   else
+#     current = katz_deli.shift
+#     puts "Currently serving #{current}."
+#   end
+# end
+
+# second attempt (7/2/17)
+katz_deli = []
+
 def line(katz_deli)
-  if katz_deli.length == 0
+  if katz_deli == []
     puts "The line is currently empty."
   else
-    deli_line = "The line is currently: "
+    line = "The line is currently: "
     katz_deli.each_with_index do |person, index|
-      deli_line += "#{index+1}. #{person} "
+      line += "#{index+1}. #{person} "
     end
-    puts deli_line.strip!
+    puts line.strip
   end
 end
 
-#add a person to the end of the line
-def take_a_number(katz_deli, name)
-  katz_deli.push(name)
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+def take_a_number(katz_deli, person)
+  katz_deli.push(person)
+  puts "Welcome, #{person}. You are number #{katz_deli.length} in line."
 end
 
-#tells you who is currently being served
 def now_serving(katz_deli)
-  if katz_deli.length == 0
+  if katz_deli == []
     puts "There is nobody waiting to be served!"
   else
-    current = katz_deli.shift
-    puts "Currently serving #{current}."
+    puts "Currently serving #{katz_deli.shift}."
   end
 end
