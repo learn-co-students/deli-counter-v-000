7e9fabddb6b3b5eb9fbcb0a3be3edd293679af55
haroon Azhar Khan <haroon1azhar@gmail.com>
2017-07-15 12:20:56 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+def line(deli)
+  c=0
+
+    if !(deli[c].class==String  )
+      puts "The line is currently empty."
+      return
+    end
+    srinn=""
+    while (c<deli.length)
+    srinn+=" #{c+1}. #{deli[c]}"
+    c+=1
+    end
+    puts  "The line is currently:#{srinn}"
+end
+def take_a_number(deli, person)
+     deli.push(person)
+     puts "Welcome, #{person}. You are number #{deli.length} in line."
+
+end
+def now_serving(deli)
+ if deli[0]==nil
+   puts "There is nobody waiting to be served!"
+   return
+  end
+  puts "Currently serving #{deli.shift()}."
+
+
+
+end
