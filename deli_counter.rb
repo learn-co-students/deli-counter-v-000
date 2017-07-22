8119e0cc09b21da775915c03cf3d56608b4d0562
lauraszyms <lauraszyms@gmail.com>
2017-07-21 21:00:59 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+katz_deli = []
+
+def line(deli_line)
+  line_array = []
+  if deli_line.size == 0
+    puts "The line is currently empty."
+  else
+   deli_line.each_with_index { |name, index| line_array << "The line is currently: #{index += 1}. #{name}" }
+  return line_array
+ end
+end
+
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+return "Welcome, #{name}. You are number #{katz_deli.size} in line."
+
+end
