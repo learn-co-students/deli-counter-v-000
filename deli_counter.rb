95b51937ddd1f0e72b9aa3a5db6ee81f003b828d
github username <github email address>
2017-08-13 23:47:00 -0700

    line def improved

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,23 @@
 katz_deli = ["Ada", "Grace", "Kent"]
 
+# def line(katz_deli)
+#   if katz_deli.empty?
+#     puts "The line is currently empty."
+#   else
+#     queue = katz_deli.each_with_index.map{|person, index| "#{index + 1}. #{person}"}
+#     puts "The line is currently: #{queue.join(" ")}"
+#   end
+# end
+
 def line(katz_deli)
-  if katz_deli.length == 0
+  if katz_deli.empty?
     puts "The line is currently empty."
   else
-    queue = katz_deli.each_with_index.map{|person, index| "#{index + 1}. #{person}"}
-    puts "The line is currently: #{queue.join(" ")}"
+    current_line = "The line is currently:"
+    katz_deli.each_with_index {|person, i| current_line << " #{i + 1}. #{person}"}
+    puts current_line
   end
+
 end
 
 def take_a_number(katz_deli, new_person)
@@ -16,9 +27,10 @@ def take_a_number(katz_deli, new_person)
 end
 
 def now_serving(katz_deli)
-  if katz_deli.length == 0
+  if katz_deli.empty?
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{katz_deli.shift}."
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
   end
 end
