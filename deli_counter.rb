8bff8f59effc6f11b8ae23a94421f0212fb74e87
Nicole Therien <nicoletherien@yahoo.com>
2017-09-01 23:48:57 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,20 +2,25 @@ def line(array)
   if array == []
     puts "The line is currently empty."
   else
-    array.map_with_index do |name, index|
-      puts "The line is currently: "
+    line = ""
+    array.each_with_index do |name, index|
+      line << " #{index + 1}. #{name}"
     end
+    puts "The line is currently:#{line}"
   end
 end
 
+
 def take_a_number(array, name)
-  if array > 1
-    new_array=[]
-    array.each_with_index do |name, index|
-      new_array << "Welcome, #{name}. You are number #{index + 1} in line."
-      new_array
-    end
-  elsif array < 1
-    "There is nobody waiting to be served!"
+  array.push("#{name}")
+  puts "Welcome, #{name}. You are number #{array.length} in line."
+end
+
+def now_serving(array)
+  if array == []
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array.first}."
+    array.shift
   end
 end
