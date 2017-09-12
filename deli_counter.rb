1dd3ff2e443cca7f58eabee2e2b937ad95ed9907
Gevanstron <gen.athena.evans@gmail.com>
2017-09-12 21:46:01 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.size > 0
+    katz_deli.each_with_index.collect do |name, number|
+      katz_deli[number] = " #{number + 1}. #{name}"
+    end
+    puts "The line is currently:#{katz_deli.join}"
+  else
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.count} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.shift}."
+  end
+end
