d53ac4ec754c5fd0f094945ed54e3f057d9d17fe
Melissa Rudick <mel.madrugada@gmail.com>
2017-09-19 15:22:09 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,7 @@
 
 def take_a_number(list, name)
    list << name
-   if list.length == 0
+   if list == 0
      puts "There is nobody in line."
    else
      puts "Welcome, #{name}. You are number #{list.index(name)+1} in line."
