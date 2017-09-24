095cad9c656ebcea786686a47ad1bfa244427282
Nicholas Halloran <nmhalloran@hotmail.com>
2017-09-24 23:42:52 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,16 +5,19 @@ def line(array)
   if array.length >= 1
     the_line = "The line is currently:"
     array.each_with_index do |name, index|
-      the_line << " #{index + 1}" + '. ' + "#{name}"
+      the_line << " #{index + 1}. #{name}"
     end
     puts the_line
   else
     puts "The line is currently empty."
+
+    # ALternatively, can use the  if array.empty? puts "The line is currently empty."
+
   end
 end
 
 def take_a_number(array, name)
-  array.push(name)
+  array.push(name) # array << name is preferred Ruby Style
   puts "Welcome, #{name}. You are number #{array.length} in line."
 end
 
@@ -22,7 +25,7 @@ def now_serving(array)
   if array.length == 0
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{array[0]}."
+    puts "Currently serving #{array[0]}." # Alternative wording use array.first
     array.shift
   end
 end
