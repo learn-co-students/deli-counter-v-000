6f8a1ecfbd4cc87e1e71241ea0be6d2c16b13415
Trisha Garrett <trisha.garrett@gmail.com>
2017-09-29 19:24:24 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+
+
+def line(katz_deli)
+  if katz_deli.size == 0
+    puts "The line is currently empty."
+  elsif other_deli >= 1
+     puts "The line is currently: "other_deli[0..-1].join
+
+
+  end
+end
+
+def take_a_number(katz_deli)
+
+end
+
+def now_serving(katz_deli)
+
+end
