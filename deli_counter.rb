a968e4cf85b2683ae4017528ab76f7ce7d67f321
Trisha Garrett <trisha.garrett@gmail.com>
2017-10-02 00:22:28 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,3 +18,12 @@ def take_a_number(katz_deli, name = "Ada")
     katz_deli<<name
       puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
+
+def now_serving(katz_deli)
+  if katz_deli.size == 0
+    puts "There is nobody waiting to be served!"
+  elsif katz_deli.size >= 1
+      puts "Currently serving Logan."
+  end
+  katz_deli.shift
+end
