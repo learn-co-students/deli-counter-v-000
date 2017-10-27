c9766f01a1d7de95544388229d3f41cd4382d0b9
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-10-27 21:07:15 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+def take_a_number(katzDeliLine, name)
+  katzDeliLine.push(name)
+  puts "Welcome, #{name}. You are number #{katzDeliLine.size} in line."
+end
+
+def now_serving(katzDeliLine)
+  if(katzDeliLine.size === 0)
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving " + katzDeliLine.shift() + "."
+  end
+end
+
+def line(katzDeliLine)
+  tempString = "The line is currently: "
+  if(katzDeliLine.size === 0)
+    puts "The line is currently empty."
+    return
+  else
+    counter = 0
+    katzDeliLine.each do |person|
+        tempString += "#{(counter + 1)}. #{person}"
+        if counter + 1 === katzDeliLine.size
+        else
+          tempString += " "
+        end
+        counter += 1
+      end
+    end
+    puts tempString
+end
