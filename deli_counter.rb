bcacd6757983053b22e39efce63094b558fd14f4
Elijah Marcano <emarcano143@hotmail.com>
2017-11-02 18:42:11 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,11 @@
 # Write your code here.
-def line(array)
-	if array.empty?
+def line(katz_deli)
+	if katz_deli.empty?
 	   puts "The line is currently empty."
 		 #return
 	else
   	string = []
-    array.each_with_index do |name, index|
+    katz_deli.each_with_index do |name, index|
       string <<  "#{index + 1}. #{name}"
 	  end
 		puts "The line is currently: #{string.join(" ")}"
