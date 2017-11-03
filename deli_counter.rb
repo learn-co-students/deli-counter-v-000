8b008a70aae4dcf1834143361ea10880229bc5fc
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2017-11-03 00:39:08 -0400

    Built a deli counter ticket system

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+katz_deli = []
+
+def line(line)
+  if line.empty?
+    puts "The line is currently empty."
+  else
+    line.collect!.with_index do |cust, index|
+      "#{index + 1}. " + cust
+    end
+    puts "The line is currently: " + line.join(" ")
+  end
+end
+
+def take_a_number(line, cust_name)
+  line << cust_name
+  puts "Welcome, #{cust_name}. You are number #{line.size} in line."
+end
+
+def now_serving(line)
+  if line.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{line.shift}."
+  end
+end
