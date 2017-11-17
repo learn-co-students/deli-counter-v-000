33ca20dba14b526e92103fee0d1e2571ea507cc1
cboujoukos <cboujoukos@gmail.com>
2017-11-17 18:30:58 -0500

    dinner time, still broken

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def line(deli)
+  if deli.size == 0
+    puts "The line is currently empty."
+  else
+    puts "The line is currently: #{deli.join()}" 
+end
+
+def take_a_number(deli, name)
+end
+
+def now_serving(deli)
+end
