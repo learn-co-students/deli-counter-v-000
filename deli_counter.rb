d9171c8f2473bef81b8f672f32db5c90163be7f4
Brittany Morris <brittmmendez@gmail.com>
2017-11-30 17:56:34 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+katz_deli = []
+def line(array)
+ if array.size==0
+   puts "The line is currently empty."
+ elsif array.size>0
+   array.each_with_index.map do |name,index|
+     puts "Welcome, #{name}. You are number #{index} in line."
+   end
+ end
