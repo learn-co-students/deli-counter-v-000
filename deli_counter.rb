ad232e869502d42f3b40a0f9ae45e03cdf52a442
Wayne Buxton <waynebuxton2184@yahoo.com>
2017-12-03 17:38:38 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+require "pry"
+def line(katz_deli)
+  name_order = []
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  elsif katz_deli.each_with_index do |name,index|
+    name_order << "#{index + 1}." "#{name}"
+  end
+    puts "The line is currently: #{name_order.join("")}"
+  end
+end
+
+#def take_a_number(katz_deli,name)
+#  katz_deli << name
+#    if katz_deli.each_with_index do |name,index|
+#    end
