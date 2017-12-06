8be81056a8ef5101d336f60c79fbe8c54573a78e
Chao Hsuan Chi <allen0413@gmail.com>
2017-12-06 10:58:52 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(array)
     puts "The line is currently empty."
   else
     current_line = "The line is currently:"
-    deli.each.with_index(1) do |person, i|
+    array.each.with_index(1) do |person, i|
         current_line << " #{i}. #{person}"
     end
     puts current_line
