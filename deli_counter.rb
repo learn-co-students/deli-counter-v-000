7dd8c74336b2fa4bbfb19e7df430c1dd6a5e49cc
funemployeddad <funemployeddad@gmail.com>
2017-12-13 16:52:29 -0700

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+
+def line(deli)
+  line = ""
+  deli.each { |p| line = "#{line} #{deli.index(p)+1}. #{p}"}
+  str = deli.empty?? "The line is currently empty.":"The line is currently:#{line}"
+  puts str
+end
+
+
+
+
+
+
+
+def take_a_number(array, string)
+
+end
+
+def now_serving
+
+end
