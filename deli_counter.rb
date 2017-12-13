5e6d07237e7316eb3b9eb42b138957350b541f84
Jonathan Dust <d24dust@hotmail.com>
2017-12-13 20:39:09 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,31 @@
+require 'pry'
 # Write your code here.
+katz_deli = ["Joules", "Jon", "Rache"]
+
+def line(names)
+  case names.size
+  when 0
+    puts "The line is currently empty."
+  else
+    str = "The line is currently:"
+    names.each_with_index do |name, index|
+      str += " #{index + 1}. #{name}"
+    end
+    puts str
+  end
+end
+
+def take_a_number(array, name)
+  array.push(name)
+  puts "Welcome, #{name}. You are number #{array.size} in line."
+end
+
+def now_serving(names)
+  case names.size
+  when 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{names[0]}."
+    names.shift
+  end
+end
