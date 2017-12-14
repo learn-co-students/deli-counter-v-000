d8b40478d3d7700491d463b34d154cf313bd3f43
Alpha Daniel <daniel.nunez.nyc@gmail.com>
2017-12-14 02:19:53 +0000

    Using .each with {}, pulling index position with .index(), using puts with Ternary operator

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def line(deli)
+  line = ""
+  deli.each { |p| line = "#{line} #{deli.index(p)+1}. #{p}" }
+  str = deli.empty?? "The line is currently empty.":"The line is currently:#{line}"
+  puts str
+end
+
+def take_a_number(deli, p)
+  deli << p
+  puts "Welcome, #{p}. You are number #{deli.index(p)+1} in line."
+end
+
+def now_serving(deli)
+  str = deli.empty?? "There is nobody waiting to be served!":"Currently serving #{deli.first}."
+  puts str
+  deli.shift()
+end
