3f80835f9bdbaf78a6c9fb1b8b208446eae5abcc
Ron Sala <ron.sala@gmail.com>
2017-12-16 07:25:45 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     katz_deli.collect.with_index { |x, i| }
-    puts "The line is currently: " + { katz_deli.each { katz_deli
+    puts "The line is currently: " + { katz_deli.each { katz_deli }
   end
 end
 
@@ -13,6 +13,6 @@ def take_a_number(katz_deli, name)
   number_list = katz_deli.each { }
 end
 
-def now_serving
-  katz_deli.empty? ? (puts "There is nobody waiting to be served!") : (puts "Currently serving #{ katz_deli[0 + 1] }.")
-end
+# def now_serving
+#   katz_deli.empty? ? (puts "There is nobody waiting to be served!") : (puts "Currently serving #{ katz_deli[0 + 1] }.")
+# end
