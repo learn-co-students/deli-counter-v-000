75e31907362cc748a6a8abf647c85790f60a93e1
Aubrey Neimeier <neimeier.1@osu.edu>
2017-12-18 23:47:25 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,6 +6,20 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     string_1 = "The line is currently:"
+      katz_deli.each_with_index do |name, index|
+        string_1 << " #{index + 1}. #{name}"
+      end
+      puts string_1
+    end
+end
+#my original line method was a bit heavy/clunky. we don't need to define a new array
+#simply shovel the "each_with_index string" onto the end of string_1
+=begin
+def line(katz_deli)
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else
+    string_1 = "The line is currently:"
     cust_position = []
       katz_deli.each_with_index do |name, index|
         cust_position[index] = " #{index + 1}. #{name}"
@@ -14,7 +28,7 @@ def line(katz_deli)
     string_1 + cust_position.join
   end
 end
-#retun value is string and the string is also puts-ed.
+=end
 
 
 def take_a_number(katz_deli, name)
@@ -30,3 +44,33 @@ def now_serving(katz_deli)
     katz_deli.shift
   end
 end
+
+
+#formal solution is
+=begin
+def line(deli)
+  if deli.empty?
+    puts "The line is currently empty."
+  else
+    current_line = "The line is currently:"
+    deli.each.with_index(1) do |person, i|
+      current_line << " #{i}. #{person}"
+    end
+    puts current_line
+  end
+end
+
+def take_a_number(deli, name)
+  deli << name
+  puts "Welcome, #{name}. You are number #{deli.length} in line."
+end
+
+def now_serving(deli)
+  if deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{deli.first}."
+    deli.shift
+  end
+end
+=end
