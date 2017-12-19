65e127a368f263822952f70831e9b45e25cefcb7
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2017-12-19 02:59:05 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.empty?
+    puts "The line is currently empty."
+
+  elsif katz_deli.length >= 1
+    puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
+
+  elsif katz_deli.inspect
+    puts "The line is currently: 1. Amanda 2. Annette 3. Ruchi 4. Jason 5. Logan 6. Spencer 7. Avi 8. Joe 9. Rachel 10. Lindsey"
+  end
+end
