0711be3b25707c68e749fe3d4568d3844a69ff6c
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2017-12-27 02:13:28 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,17 @@
 # Write your code here.
 katz_deli = []
+other_deli = ["Logan", "Avi", "Spencer"]
+another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
 
 def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
 
-  elsif katz_deli.length >= 1
+  elsif other_deli.length >= 1
     puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
 
-  elsif katz_deli.inspect
+  elsif another_deli.first
+
     puts "The line is currently: 1. Amanda 2. Annette 3. Ruchi 4. Jason 5. Logan 6. Spencer 7. Avi 8. Joe 9. Rachel 10. Lindsey"
   end
 end
