3c2a36a66f22f49c75bb89c3610ff24b5370fcf4
neodynia <neodynia@icloud.com>
2017-12-28 07:49:22 -0500

    done

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,9 @@
-# Write your code here.
-
-
-git 
+def line (katz_deli)
+  if katz_deli.count==0
+    puts "The line is currently empty."
+  else katz_deli
+    katz_deli.each_with_index do |name, i|
+      puts "The line is currently: #{i+1}. #{katz_deli[i]}"
+    end
+  end
+end
