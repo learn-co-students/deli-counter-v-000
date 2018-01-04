7535b7c03f1f68fd12fc64052007622b29808989
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-01-04 01:02:08 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,17 +1,30 @@
 # Write your code here.
 katz_deli = []
-other_deli = ["Logan", "Avi", "Spencer"]
-another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
 
 def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
 
-  elsif other_deli.length >= 1
-    puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
+  else
+    current_line = "The line is currently:"
+    katz_deli.each.with_index(1) do |person, i|
+      current_line << " #{i}. #{person}"
+  end
+  puts current_line
+  end
+end
 
-  elsif another_deli.first
+def take_a_number(katz_deli, name)
+  #return name and place in line
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
 
-    puts "The line is currently: 1. Amanda 2. Annette 3. Ruchi 4. Jason 5. Logan 6. Spencer 7. Avi 8. Joe 9. Rachel 10. Lindsey"
+def now_serving(katz_deli)
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
   end
 end
