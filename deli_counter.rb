d078692317e7abe70a1a2d30f70ed0ba90d54959
Alli Tzadok <allisontzadok@gmail.com>
2018-01-07 00:32:01 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+katz_deli = []
+
+def line(deli)
+  if deli.length == 0
+    puts "The line is currently empty."
+  else
+    current_line = "The line is currently:"
+    deli.each.with_index(1) do |name, i|
+      current_line << " #{i}. #{name}"
+    end
+    puts current_line
+  end
+end
