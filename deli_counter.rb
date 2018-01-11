4cf7a3fea7c16ff899772173c4e7386de920740c
jacobsilver2 <jacobsilver2@mac.com>
2018-01-10 22:53:31 -0500

    completed first two methods

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+require 'pry'
+
+katz_deli = ["John", "Jacob"]
+
+def line(currentLine)
+   msg = "The line is currently:"
+   if currentLine.length == 0
+      puts "The line is currently empty."
+   else
+      currentLine.each_with_index do |person, index|
+         msg << " #{index + 1}. #{person}"
+      end
+      puts msg
+   end
+end
+
+def take_a_number(currentLine, name)
+   currentLine << name
+   puts "Welcome, #{name}. You are number #{currentLine.size} in line."
+end
+
+def now_serving
+end
+
