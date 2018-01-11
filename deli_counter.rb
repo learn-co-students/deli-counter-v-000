4f4f341466a80a2bda1eda8a2966abfb48faca7a
A T Williams <aderiannaw@yahoo.com>
2018-01-11 04:52:57 +0000

    still working on #line method part 2

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+katz_deli = []
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+ puts "The line is currently: #{katz_deli}" 
+  end
+end
+
+def take_a_number(katz, name)
+  katz.push(name)
+puts "Welcome, #{name}. You are number #{katz.length} in line."
+
+end
+
+def now_serving
+if katz.length == 0
+    puts "There is nobody waiting to be served!"
+  elsif
+     katz >= 1
+    puts "Currently serving #{name}."
+    katz -= 1
+end
+end
