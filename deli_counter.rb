7ecb64195cbbca6cdecda8536d551f482a22843c
Berkeley Fahrenthold <berkeley_fahrenthold@urmc.rochester.edu>
2018-01-11 17:33:47 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,16 @@
 katz_deli = []
+
 def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
     customers = "The line is currently:"
     katz_deli.each.with_index(1) do |person, index|
-      customers << " #{index}. #{person}"
-    end
-    puts customers
+    customers  << " #{index}. #{person}"
+  end
+  puts customers
   end
 end
-
 
 def take_a_number(katz_deli, name)
   katz_deli << name
