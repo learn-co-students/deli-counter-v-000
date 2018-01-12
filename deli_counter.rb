b93417ff473b6df7caa3c3a30451d98945f262e6
jljardon <github email address>
2018-01-12 01:08:49 -0500

    add deli methods

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def line(katz_deli)
+   line = 'The line is currently:'
+   katz_deli.each_with_index.map { |name, numer_in_line| line += " #{numer_in_line + 1}. #{name}" }
+   puts(katz_deli.empty? ? 'The line is currently empty.' : line)
+end
+
+def take_a_number(katz_deli, name)
+   katz_deli.push(name)
+   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(katz_deli)
+   puts (!katz_deli.empty? ? "Currently serving #{katz_deli[0]}." : "There is nobody waiting to be served!")
+   katz_deli.shift
+end
