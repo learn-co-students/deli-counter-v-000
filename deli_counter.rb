dd309454a09607eaf3844e7675e699ad7ede2af8
jpkim921 <jpkim921@gmail.com>
2018-01-15 20:27:11 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def line(array)
+
+  line_arr = array.map.each_with_index { |e,i| "#{i + 1}. #{e}"}
+
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+    puts "The line is currently: #{line_arr.join(" ")}"
+  end
+
+end
+
+
+def take_a_number(katz_deli, name)
+
+end
