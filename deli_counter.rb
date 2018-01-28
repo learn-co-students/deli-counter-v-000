35382147c0e507e863efd867885dbe5eaa1b914e
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-01-28 07:05:54 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+def line(array)
+  line_order = [ ]
+  if (array.size == 0)
+    puts "The line is currently empty."
+  else
+    count = 0
+    while count < array.size
+      line_order.push("#{(count + 1).to_s}. #{array[count]}")
+      count += 1
+    end # while loop
+
+    puts "The line is currently: #{line_order.join(" ")}"
+  end
+end ## method line
+
+
+def take_a_number(array, name)
+  array.push(name)
+  puts "Welcome, #{name}. You are number #{array.length} in line."
+end
+
+def now_serving(array)
+  if array.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array[0]}."
+    array.shift
+  end
+end
