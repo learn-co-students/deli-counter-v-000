f3bee5ba600e36c51a9e52b3b370d5d46cd8e2b6
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-02-04 17:08:04 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,34 @@
 # Write your code here.
+
+def line(katz_deli)
+  string  = ""
+  if katz_deli.length == 0
+    string = "The line is currently empty."
+  else
+    string = "The line is currently:"
+    katz_deli.each_with_index do |element, index|
+      string = string + " #{index + 1}. " + element
+    end
+  end
+  puts string
+end
+
+def take_a_number(katz_deli, name)
+  if katz_deli.include?(name) == false
+        katz_deli.push(name)
+  end
+  katz_deli.each_with_index do |element, index|
+    if name == element
+      puts "Welcome, #{name}. You are number #{index + 1} in line."
+    end
+  end
+end
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
+  end
+end
