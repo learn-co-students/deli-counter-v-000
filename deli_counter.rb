6dcb06125d193c7c208980f353d823e21a3e9129
Hiroki Kato <hkato@sas.upenn.edu>
2018-02-12 02:54:09 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli.count == 0
+    puts "The line is currently empty."
+  else
+    line_order = ["The line is currently:"]
+    katz_deli.each_with_index do |a, b|
+      line_order << "#{b + 1}. #{a}"
+    end
+  puts line_order.join(" ")
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.count} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.count == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.shift}."
+  end
+end
