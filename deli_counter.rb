acc9f471fc4b8ce611beca7396606944ece15966
Katie Cassell <kcassell944@gmail.com>
2018-02-15 20:40:35 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def line(katz_deli)
+if katz_deli.count == 0
+  puts "The line is currently empty."
+else
+  output =  "The line is currently:"
+  katz_deli.each_with_index do |person, index|
+    output<< " #{index+1}. #{ person }"
+end
+puts output
+end
+end
+def take_a_number(katz_deli, person)
+end
