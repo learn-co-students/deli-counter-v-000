4278f4d1fee349cd3564a240d575dedabff2ce66
Katie Cassell <kcassell944@gmail.com>
2018-02-16 15:18:11 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,4 +10,14 @@ puts output
 end
 end
 def take_a_number(katz_deli, person)
+  katz_deli.push(person)
+  puts "Welcome, #{ person }. You are number #{ katz_deli.length } in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.count == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{ katz_deli.shift }."
+  end
 end
