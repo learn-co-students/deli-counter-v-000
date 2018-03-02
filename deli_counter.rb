82ecaae796dc8f1cb65f7ff6513c558dad926d3d
Aspen James <aspenjames@tqca.org>
2018-03-02 21:49:29 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+def line(deli_line)
+  if deli_line == []
+    puts "The line is currently empty."
+  else
+    line_with_spot = Array.new
+    deli_line.each_with_index { |name, index|
+      spot = index + 1
+      line_with_spot << "#{spot}. #{name}"
+    }
+    puts "The line is currently: #{line_with_spot.join(" ")}"
+  end
+end
+
+def take_a_number(deli_line, *name)
+  name = name.join
+  deli_line << name
+  puts "Welcome, #{name}. You are number #{deli_line.length} in line."
+end
+
+def now_serving(deli_line)
+  if deli_line == []
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{deli_line[0]}."
+    deli_line.shift
+  end
+end
