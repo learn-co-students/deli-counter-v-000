0d534df322d07cde59843e74106605945fa6532e
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-03-26 23:50:24 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+
+def line (deli)
+  if deli.size == 0
+    puts "The line is currently empty."
+  else
+    str = "The line is currently:"
+    deli.each_with_index { |name, i| str << " #{i+1}. #{name}"}
+    puts str
+  end
+end
+
+def take_a_number (deli, name)
+  deli << name
+  puts "Welcome, #{name}. You are number #{deli.size} in line."
+end
+
+def now_serving (deli)
+  if deli.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    customer = deli.shift
+    puts "Currently serving #{customer}."
+  end
+end
