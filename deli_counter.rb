214d966cd779630d4e4ebaa581a6e475685180b5
Nicholas Bharucha <nickbharucha@ou.edu>
2018-04-10 22:58:05 +0000

    Added several methods to deli_counter.rb file

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def line(array)
+  if !array.any?
+    puts "The line is currently empty."
+  else
+    new_array = []
+    array.each_with_index do |name, index|
+      new_array[index] = "#{index + 1}. #{name}"
+    end
+    new_string = new_array.join(" ")
+    puts "The line is currently: #{new_string}"
+  end
+end
+
+def take_a_number(array, name)
+  array << name
+  puts "Welcome, #{name}. You are number #{array.length} in line."
+end
+
+def now_serving(array)
+  if !array.any?
+    puts "There is nobody waiting to be served!"
+  else
+  puts "Currently serving #{array[0]}."
+  array.shift
+  end
+end
