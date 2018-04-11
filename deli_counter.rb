2afb599d1f110b2120cbc9f721edc390223227fe
Anuradha Khambete <anukhambete@gmail.com>
2018-04-11 18:35:23 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+
+def line(array_customers)
+  num = array_customers.size
+  if num == 0
+    puts "The line is currently empty."
+  else
+   arr_line = array_customers.collect.with_index {|cust,index| "#{index+1}. #{cust}"}
+   arr_line.insert(0,"The line is currently:")
+   puts arr_line.join(" ")
+  end
+end
+
+#def take_a_number(katz_deli,newname)
+ # puts "Welcome,#{newname}. You are number #{index+1}"
+#end
+
