cdfcd007642f49ac773e45a35be638aa55a7bd9a
Brent Bauman <brentbauman86@gmail.com>
2018-04-17 17:05:48 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+if katz_deli == 0
+  puts "The line is currently empty."
+else
+  puts "You're next in line."
+end
+
+def take_a_number()
+end
