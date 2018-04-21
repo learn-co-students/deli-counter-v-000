668decab7bcbf9133acde1f80e9a300e0ae3374c
Christia T <christiatidwell@bellsouth.net>
2018-04-21 21:32:52 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,6 @@ def line(deli = katz_deli)
     puts "The line is currently empty."
   else
     message="The line is currently:"
-
     katz_deli.each_with_index do |value, index|
       message += " #{index.to_i+1}. #{value}"
     end
