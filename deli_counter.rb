b5e10ce188df270c1156f9be863e8d51cbfaad55
Christia T <christiatidwell@bellsouth.net>
2018-04-21 21:03:29 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,6 +7,6 @@ def line(deli = katz_deli)
   else
     line_numbers = [ 1,2,3,4,5,6,7,8,9,10]
     (line_numbers.zip(deli)).join(" , ") = string
-    puts "The line is currently #{ }."
+    puts "The line is currently #{string }."
   end
 end
