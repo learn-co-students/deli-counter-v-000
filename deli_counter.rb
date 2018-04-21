469438dfe32f5e459fba13d786dc94e0369039cd
Christia T <christiatidwell@bellsouth.net>
2018-04-21 21:01:07 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,6 +7,6 @@ def line(deli = katz_deli)
   else
     line_numbers = [ 1,2,3,4,5,6,7,8,9,10]
     line_numbers.zip(deli) == deli
-    puts "The line is currently #{(line_numbers.zip(deli)_.join(" , ")}."
+    puts "The line is currently #{(line_numbers.zip(deli).join(" , ")}."
   end
 end
