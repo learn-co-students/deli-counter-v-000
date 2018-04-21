9f137687125b9b06d34c7eb6a1bb913b017d798e
Christia T <christiatidwell@bellsouth.net>
2018-04-21 21:10:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,6 +6,7 @@ def line(deli = katz_deli)
     puts "The line is currently empty."
   else
     line_numbers = [ 1,2,3,4,5,6,7,8,9,10]
+    line_numbers.length = deli.length
     (line_numbers.zip(deli)).join(" , ") == String
     String.length === deli.length
     puts "The line is currently #{String}."
