cc2a80164d0e1ed4e26790faf407d5c531a0d0a0
Christia T <christiatidwell@bellsouth.net>
2018-04-21 21:09:57 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(deli = katz_deli)
   else
     line_numbers = [ 1,2,3,4,5,6,7,8,9,10]
     (line_numbers.zip(deli)).join(" , ") == String
-    String.length = deli.length
+    String.length === deli.length
     puts "The line is currently #{String}."
   end
 end
