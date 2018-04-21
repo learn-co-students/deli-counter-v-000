3d1bfe5e2b0b2cece2a14ae1a32cea46ac7f4bc5
Christia T <christiatidwell@bellsouth.net>
2018-04-21 21:24:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,10 +5,13 @@ def line(deli = katz_deli)
   if deli.length == 0
     puts "The line is currently empty."
   else
-    line_numbers = [ 1,2,3,4,5,6,7,8,9,10]
-    String == (line_numbers.zip(deli)).join(" , ")
-    if deli.size < line_numbers,
-      line_number.drop(deli.size)
-    puts "The line is currently #{String}."
+  message="The line is currently:"
+
+  katz_deli.each_with_index do |value, index|
+    message += " #{index.to_i+1}. #{value}"
+  end
+
+  puts "#{message}"
+
   end
 end
