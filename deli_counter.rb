883b44c13802f78836c4d4a82777316920ff46c1
Christia T <christiatidwell@bellsouth.net>
2018-04-21 21:46:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,6 +14,6 @@ def line(deli = katz_deli)
 end
 
 def take_a_number(katz_deli, value)
-  puts "katz_deli.shift(index.to_i+1)"
+  puts "Welcome #{value}.  You are number #{index.to_i+1} in line."
 
 end
