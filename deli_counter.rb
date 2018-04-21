c068c2311f979fea2c736051e0e93bc64bca4ebb
Christia T <christiatidwell@bellsouth.net>
2018-04-21 20:22:21 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+
+def line(deli = katz_deli)
+
+  if deli.length == 0
+    puts "The line is currently empty."
+  else
+    var queue = [];
+         deli.forEach(function(person, index){
+            queue.push((index + 1).toString() + ". " + person);
+          });
+          var response = "The line is currently: " + queue.join(", ")
+
+
+    puts "The line is currently #{deli.join(" , ")}."
+ end
+end
+
+
+def take_a_number
+
+end
+
+
+def now_serving
+
+end
