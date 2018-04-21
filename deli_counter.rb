b4c67c281c0e3aa6a09681943497f1814ad92fae
Christia T <christiatidwell@bellsouth.net>
2018-04-21 21:56:35 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,6 +17,6 @@ def take_a_number(katz_deli, name)
   if katz_deli == []
   puts "Welcome #{name}. You are number 1 in line."
 else
-  puts "Welcome #{name}.  You are number #{index.to_i+1} in line."
+  puts "Welcome #{name}.  You are number #{katz_deli[-1]} in line."
   end
 end
