1c3904f0cff4989a35bd07f46713623ddd996cbf
Dale Terrell <tragicid@gmail.com>
2018-05-04 14:53:32 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,12 @@
 def line(katz_deli) 
   counter = 0
-  line(katz_deli).each do |katz_deli|
+  katz_deli.each do |katz_deli|
     if counter == 0
-      puts "there is nobody in line"
-    puts #{katz_deli}
+      puts ("The line is currently empty.")
+    else
+      puts #{katz_deli}
   counter += 1
+end
 end
 end
 
