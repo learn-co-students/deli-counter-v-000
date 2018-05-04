36b1aab09250d841d52ffdf6e238a2bfea0655e6
Dale Terrell <tragicid@gmail.com>
2018-05-04 14:40:27 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,10 @@
-katz_deli = []
-def line = []
-  line += 1
-  puts 
+def line(katz_deli) 
+  counter = 0
+  line(katz_deli) each.do |katz_deli|
+    if counter == 0
+      puts "there is nobody in line"
+    puts #{katz_deli}
+  counter += 1
+end
 end
 
-def take_a_number
-  puts katz_deli
-  puts take a number(name)
-  puts #{name, katz_deli}
