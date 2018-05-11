41f754d9108c7408fd8d29ac58b55d8a4ce3109e
Carrie Savinell <carrielane89@gmail.com>
2018-05-11 17:53:59 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,30 +1,29 @@
 require 'pry'
 katz_deli = []
 
-def line(katz_deli)
-  if katz_deli.length == 0
+def line(in_line_array)
+  if in_line_array.length == 0 
     puts "The line is currently empty."
-  else
-    message_array = []
-    katz_deli.each_with_index do |name, index|
-     message_array << ("#{index + 1}. #{name}")
-     #binding.pry
-     #push method pushes name into index
-    end
-  puts "The line is currently: #{message_array.join(" ")}" 
- end  
+  else 
+    new_array = []
+    in_line_array.each_with_index do |name, index|
+      new_array << ("#{index + 1}. #{name}")
+    end 
+    puts "The line is currently: #{new_array.join(" ")}"
+  end   
 end   
 
-def take_a_number(katz_deli, name)
-  katz_deli << (name) 
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
-end  
 
-def now_serving(next_person)
-  if next_person.empty? 
+def take_a_number(katz_deli, new_customer)
+  katz_deli << new_customer
+    puts "Welcome, #{new_customer}. You are number #{katz_deli.length} in line."
+end 
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0 
     puts "There is nobody waiting to be served!"
-  else
-    puts "Currently serving #{next_person[0]}."
-    next_person.shift
-  end 
-end   
+  else 
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift
+  end   
+end 
