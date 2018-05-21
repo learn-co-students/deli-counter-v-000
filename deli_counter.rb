6797b76cbb0e145edd31731c5b9d85e5cce4528e
Elaine Anderson <eparie@bu.edu>
2018-05-21 22:40:52 +0000

    serving customers

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,11 +2,28 @@
 require 'pry'
 
 def line(array)
-  customer_line = []
-if array.length > 0
-  array.each.with_index(1) do |name, index|
-   customer_line.push("#{index}. #{name}")
+  customer_line = String.new
+  if array.length > 0
+    array.each.with_index(1) do |name, index|
+      customer_line << "#{index}. #{name} "
+    end
+    puts "The line is currently: #{customer_line}".chop
+  else
+    puts "The line is currently empty."
   end
-  puts "The line is currently: #{customer_line}"
 end
+
+
+def take_a_number(array, name)
+array << "#{name}"
+  puts "Welcome, #{name}. You are number #{array.length} in line."
+  end
+
+  def now_serving(array)
+    if array.length > 0
+    puts "Currently serving #{array[0]}."
+    array.shift
+  elsif
+    puts "There is nobody waiting to be served!"
+  end
 end
