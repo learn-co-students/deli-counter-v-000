94eac5a08135022f29342345143351164ff2cd78
Elaine Anderson <eparie@bu.edu>
2018-05-21 19:22:45 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,9 @@
 # Write your code here.
+
+def line(array)
+if array.length > 0
+  new_array = array.collect.with_index(1) do |name, index|
+    puts "The line is currently: #{index}. #{name}"
+  end
+end
+end
