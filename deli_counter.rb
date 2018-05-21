40507ffc030ce7579c736352822287bd110d13cb
Elaine Anderson <eparie@bu.edu>
2018-05-21 19:32:09 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,12 @@
 # Write your code here.
+require 'pry'
 
 def line(array)
+  customer_line = []
 if array.length > 0
-  new_array = array.collect.with_index(1) do |name, index|
-    puts "The line is currently: #{index}. #{name}"
+  array.each.with_index(1) do |name, index|
+   customer_line.push("#{index}. #{name}")
   end
+  puts "The line is currently: #{customer_line}"
 end
 end
