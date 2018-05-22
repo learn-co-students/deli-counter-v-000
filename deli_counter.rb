404f973e3aa74e331190d355a1e6422eb85ca076
Jebediah Elliott <jebediahelliott@gmail.com>
2018-05-22 17:46:53 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,28 @@
+#Method to give status of the line
 def line(array)
   if array.length == 0
     puts "The line is currently empty."
   else
-    string = "The line is currently:"
-    array.each do |name|
-      string << " #{name.index + 1}. #{name}"
+    array.collect!.with_index do |name, i|
+      name = "#{i + 1}. #{name}"
     end
-    puts string
+    array = array.join(" ")
+    puts "The line is currently: #{array}"
+  end
+end
+
+#Method welcomes customer, adds them to the line, and tells them position in line
+def take_a_number(array, name)
+  puts "Welcome, #{name}. You are number #{array.length + 1} in line."
+  array << name
+end
+
+#
+def now_serving(array)
+  if array.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array[0]}."
+    array.shift
   end
 end
