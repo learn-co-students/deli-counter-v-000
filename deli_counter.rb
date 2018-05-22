afb24070af3f96473ee7e4dc968afbad225a6bea
Jebediah Elliott <jebediahelliott@gmail.com>
2018-05-22 16:25:52 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+    string = "The line is currently:"
+    array.each do |name|
+      string << " #{name.index + 1}. #{name}"
+    end
+    puts string
+  end
+end
