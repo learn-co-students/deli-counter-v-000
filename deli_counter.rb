2edc47efd6eb94353690d0a59c0e0705251f4e28
jeremy.zinger@gmail.com <jeremy.zinger@gmail.com>
2018-05-29 14:23:07 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,16 +7,31 @@
 #iterate through each index and give number to people
 #now serving to iterate through integer
 
-def line(katz_deli)
+katz_deli = []
 
-
-  number_of_people_in_line = line_length[]
-  if line_length.length == 0
-    return "The liine is currently empty."
-  elsif line_length.length >= 1
-    return "You are "
+def line(number_of_people_in_line)
+  line = []
+  if number_of_people_in_line.length = 0
+    return "The line is currently empty."
+  else
+    line.each.with_index(0) do | name, index |
+      line.push("#{index}. #{name}")
+    end
+  end
+  puts "The line is: #{line.join(" ")}"
 end
 
 def take_a_number(katz_deli, name)
+   katz_deli.push(name)
+   puts "Welcome #{name}.  You are currently #{katz_deli.length} in line."
+end
+
+def now_serving(next_in_line)
+  if next_in_line.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving customer number #{next_in_line[0]}."
+    next_in_line.shift
+  end
+end
   
-  puts "#{name}"
