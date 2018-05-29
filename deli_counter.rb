d1fb71245a120f413cfebc51f9b625201ad9dcea
jeremy.zinger@gmail.com <jeremy.zinger@gmail.com>
2018-05-29 14:56:14 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(number_of_people_in_line)
   if number_of_people_in_line.length == 0
     puts "The line is currently empty."
   else
-    line.each.with_index(1) do | name, index |
+    line.each.with_index(0) do | name, index |
       line.push("#{index}. #{name}")
     end
     puts "The line is currently: #{line.join(" ")}"
