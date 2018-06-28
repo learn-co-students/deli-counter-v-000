1de2db5f2ce861a84b3fe8b2e99f7fe5ba5b1561
Jamie Brown <jmbrowncow@gmail.com>
2018-06-28 22:57:42 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,10 +3,10 @@ def line(katz_deli)
   if katz_deli.count == 0
     puts "The line is currently empty."
   else
+    current_line = "The line is currently:"
     katz_deli.each_with_index{|name, index|
-    katz_deli[index] = "#{index+1}. #{name}"}
-    line_to_string =katz_deli.join(" ")
-    puts "The line is currently: #{line_to_string}"
+    current_line << " #{index+1}. #{name}"}
+    puts current_line
   end
 end
 # Write your code here.
