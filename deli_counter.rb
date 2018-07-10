4f164e62140a24b544b03cb0d8ab11708494ed30
Bailey Sullivan <bailey.rose.sullivan@gmail.com>
2018-07-10 22:48:23 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,5 @@
 def line(people)
-  if people.length >=1
+  if people.length >= 1
     arr = []
     people.each_with_index do |person, index|
       arr << " #{index + 1}. #{person}"
@@ -9,3 +9,17 @@ def line(people)
     puts "The line is currently empty."
   end
 end
+
+def take_a_number(katz_deli, new_customer)
+  katz_deli << new_customer
+  if katz_deli.length >=1 
+    puts "Welcome, #{new_customer}. You are number #{katz_deli.length} in line."
+  else 
+    puts "The line is empty."
+  end
+end
+
+  
+
+
+  
