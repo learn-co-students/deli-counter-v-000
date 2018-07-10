69c551638e9adfa8f024f5235a452088c95ef288
Bailey Sullivan <bailey.rose.sullivan@gmail.com>
2018-07-10 20:49:47 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+def line(people)
+  if people.length < 1
+    puts "The line is currently empty."
+  end
+  else 
+    arr = []
+    people.each_with_index do |person, index|
+      arr << " #{index + 1}. #{person}"
+    end
+     puts "The line is currently:#{arr.join}"
+  end
