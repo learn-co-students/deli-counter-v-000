e3c5ed47e791775de3c8c98c30b644c4e9af8d9a
Rafael Lopez <lpz.raf@gmail.com>
2018-07-21 16:18:42 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def line(deli) 
+  if deli.empty? == true 
+    puts "The line is currently empty."
+  else 
+  deli.each_with_index do |position, name|
+  puts `The line is currently: #{position}. #{name} #{position}. #{name} #{position}. #{name}`
+end
+end
+
+
+
+
