911808d71e9295835b03fb03a6a58bc9081f486a
J Fern <jfern20@gmail.com>
2018-08-07 23:54:07 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+
+def line(array)
+  if array.length == 0 
+    puts "The line is currently empty."
+  else 
+    counter = 1
+    other_deli.each_with_index do |name, index|
+      puts "The line is currently: #{index}. #{name}"
+      counter += 1 
+    end 
+  end 
+end 
