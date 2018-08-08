c437536c972098b92078a412a0aa0b04cfe904d6
J Fern <jfern20@gmail.com>
2018-08-08 04:20:17 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,18 +11,19 @@ def line(katz_deli)
    puts "The line is currently: #{array.join(" ")}" 
   end 
 end 
-
-def take_a_number(katz_deli,name)
+ 
+ def take_a_number(katz_deli,name)
 katz_deli << name
 puts"Welcome, #{name}. You are number #{katz_deli.length} in line."
 end 
 
-def now_serving(katz_deli)
+ def now_serving(katz_deli)
   
    if katz_deli.length == 0 
  puts"There is nobody waiting to be served!"
  
  else 
-   puts"Currently serving #{katz_deli.shift}"
+   puts"Currently serving #{katz_deli.shift}."
    #Will remove 1st name and add next on line
-end 
+  end 
+ end 
