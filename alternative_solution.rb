6119c330d80a56ab38e8bed7e10f3e9c608e5e87
Brad McCollum <btmcco8@gmail.com>
2018-08-11 21:23:29 +0000

    Done.

---
+++ alternative_solution.rb
@@ -0,0 +1,12 @@
+#removed from mainfile, documenting for informative purposes with alternative solutions originally created:
+
+# def line(array)
+#   if array.length > 0
+#     response = array.map{|name| "#{array.index(name) + 1}. #{name}"}.join(" ")
+#     response.insert(0, "The line is currently: ")
+#     puts response
+#   else
+#     puts "The line is currently empty."
+#   end
+#  end
+#        
--- deli_counter.rb
+++ deli_counter.rb
@@ -1,34 +1,18 @@
-require 'pry'
 katz_deli = []
 
-def line(array) #shows everyone in the line their current place, but if empty should tell them so
-    array.length > 0 ? puts(new_string = array.map{|name| "#{array.index(name) + 1}. #{name}"}.join(" ").insert(0, "The line is currently: ")) : puts("The line is currently empty.")
+def line(array) #displays the current line including names, or informs the user that the line is empty if so
+    array.length > 0 ? puts(response = array.map{|name| "#{array.index(name) + 1}. #{name}"}.join(" ").insert(0, "The line is currently: ")) : puts("The line is currently empty.")
 end
 
+def take_a_number(array, name) #new customers have their name added to the line and are welcomed/told their current number in line
+    array << name && puts("Welcome, #{name}. You are number #{(array.index(name) + 1)} in line.")
+end
 
-# def take_a_number()
-#
-# end
-#
-# def now_serving()
-#
-# end
-
-
-#ternary operator?
-#  new_array = []
-#
-#  array.length > 0 ? (array.each{|name| puts("The line is currently #{array.index}#{name}")}) : puts("The line is currently empty.")
-# end
-# idea: take the array and convert it to a string, push "the line is currently.." to the front of the string, and add the #s/names after
-# # new_array = []
-# new_array.push ("The line is currently: ") << array.
- # if array.length > 0
-# new_string = array.map{|name| "#{array.each_index{|number| print number}}. #{name}"}.join(" ")
-# new_string = array.map{|name| "#{array.each_index{|value, index| "#{index}"}}. #{name}"}.join(" ")
-  # new_string = array.each_with_index{ |name, index| "#{array.index(name) +1}. #{name}"}.join(" ").insert(0,"The line is currently: ")
-  #   new_string = array.map{|name| "#{array.index(name) + 1}. #{name}"}.join(" ").insert(0, "The line is currently: ")
-  #   puts new_string
-  # else
-  #   puts "The line is currently empty."
-  # end
+def now_serving(array) #informs the user who is currently being served
+  if array.length > 0
+    puts "Currently serving #{array[0]}."
+    array.shift
+  else
+    puts "There is nobody waiting to be served!"
+  end
+end
