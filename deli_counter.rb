207d49f4e6286687a5004f7f9cd9d4997ca177a0
Ashley Wong <ashwong04@gmail.com>
2018-08-29 21:02:28 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,20 @@
 # Write your code here.
 
-def line()
-  
+def line(katz_deli)
+  if katz_deli.length > 0
+    new_katz_deli = ["The line is currently:"]
+    katz_deli.each_with_index {|line, index| new_katz_deli << "#{index+1}. #{line}"}
+    puts new_katz_deli.join(" ")
+    
+  elsif katz_deli = []
+    puts "The line is currently empty."
+  end
+end
+
+
+def take_a_number(katz_deli, name)
+  #if katz_deli = []
+   # return "The line is currently empty."
   
 
 
