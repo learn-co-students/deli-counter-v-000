f89e39d67af8bd39f38f6d2b7ab113ae4a802266
Ashley Wong <ashwong04@gmail.com>
2018-08-30 15:21:04 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,9 +13,16 @@ end
 
 
 def take_a_number(katz_deli, name)
-  #if katz_deli = []
-   # return "The line is currently empty."
-  
+  katz_deli << name
+  index = katz_deli.length
+  puts "Welcome, #{name}. You are number #{index} in line."
+end
 
-
+def now_serving(katz_deli)
+  if katz_deli == []
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving " + katz_deli[0] + "."
+    katz_deli.shift
+  end
 end
