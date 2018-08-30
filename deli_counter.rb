757563bfa1065e51a0c123a078490a10dd59e4a7
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-08-30 19:00:36 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.length == 0 
+    puts "The line is currently empty."
+  elsif katz_deli.length > 0 
+    katz_deli[+1].insert
+end
+katz_deli.join
+
+
+
+def take_a_number(katz_deli, name)
+ puts "Welcome, #{name}. You are number #{katz_deli} in line."
+end
+
 # Write your code here.
