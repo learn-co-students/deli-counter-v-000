90bbe2aa4673efee3d546d65cdd74466465359a3
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-09-08 01:53:52 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,32 +1,41 @@
 # Write your code here.
+# katz = ["tina", "bob"]
 
 def line(array)
-  if array.count == 0 
-    "The line is currently empty."
+  if array.count < 1 
+    puts "The line is currently empty."
   else
-    for i in array
-    name = array[i]
-    position = i + 1
-    name_and_position = "#{position}. #{name}"
     name_of_people_in_line = []
-    name_of_people_in_line.push(name_and_position)
+    for i in array
+    name = i
+    position = array.index(name)
+    final_position = position + 1
+    name_and_position = " #{final_position}. #{name}"
+    name_of_people_in_line.push(name_and_position.to_s)
     end
-    name_of_people_in_line
+    puts "The line is currently:#{name_of_people_in_line.join()}"
   end 
 end
 
+# line(katz)
+
 def take_a_number(array, name)
-  spot_in_line = array.count + 1
-  "Welcome, #{name}. You are number #{spot_in_line} in line."
+    array.push(name)
+    spot_in_line = array.count
+    for i in array
+      if i == name 
+      puts "Welcome, #{name}. You are number #{spot_in_line} in line."
+    end
+  end
 end
   
 
-def now_serving
+def now_serving(array)
   if array.count == 0 
     puts "There is nobody waiting to be served!"
   else 
-    next_person array[0]
-    puts next_person
+    next_person = array[0]
+    puts "Currently serving #{next_person}."
     array.shift
   end
 end
