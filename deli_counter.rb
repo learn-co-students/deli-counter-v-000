e80959963f052244b883d684f48d9ec0d9877393
Guy Bryant <guy.s.bryant@gmail.com>
2018-09-09 19:20:51 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+
+katz_deli = []
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.index(name)+1}"
+end
+
+def line(katz_deli)
+  puts "The line is currently empty." if katz_deli.length < 1
+end
+
+def now_serving(katz_deli)
+  
+end
