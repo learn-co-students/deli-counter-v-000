41b99ff8221bd59c06b4ae9418649b5071243293
Amil Masic <amil.masic@gmail.com>
2018-09-12 01:48:05 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,38 @@
 # Write your code here.
+require "pry"
+def line(array)
+  # checks if the line is empty
+  if array.size == 0
+      puts "The line is currently empty."
+    else
+    # defined an empty array to store itterated values in
+    arr_line = []
+    array.each_with_index do |name, index|
+      # making index start from 1
+      index +=1
+      # pushing values into new array
+      arr_line << "#{index}. #{name}"
+    end
+    # binding.pry
+    # displaying the message while also converting array to string
+    puts "The line is currently: " + arr_line.join(" ")
+  end
+  end
+#
+def take_a_number(katz_deli, name)
+# binding.pry
+  katz_deli << name
+  # index = katz_deli.size
+
+
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift
+  end
+end
