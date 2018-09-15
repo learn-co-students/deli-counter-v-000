c394bed57590d8d5e0628bed6834cd6ca5b45606
Sherry Martin <sm674@cornell.edu>
2018-09-15 11:42:12 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+
+def line(katz_deli)
+   if katz_deli.empty?
+      puts "The line is currently empty."
+    else 
+      katz_deli.each_with_index do |name, index|
+      puts "The line is currently: #{index + 1}. #{name}"
+  end
+end
+end
+
+
+   
