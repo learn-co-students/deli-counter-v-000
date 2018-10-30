640390e632df38d8c7b361244e4cb2f5ba146613
katejsenger <katejsenger@yahoo.com>
2018-10-30 02:30:58 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+def line(array)
+  if array.length >= 1
+  a = []
+  counter = 1
+  array.each do |name|
+    a.push("#{cointer}. #{name}"") 
+    counter += 1
+  end
+else
+  a.push("The line is currently empty.")
+end
+end
+
+
