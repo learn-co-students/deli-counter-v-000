0692295279cad7953f1e019d81b210faaccb004d
Kimberly Aviles <01010110.v@gmail.com>
2018-11-07 09:18:49 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(deli_queue)
+  if deli_queue.count == 0
+    puts "The line is currently empty."
+  else deli_queue.count > 0
+    placeholdercounter = 0
+    numbers_with_names = []
+      deli_queue.each do |name|
+        placeholdercounter += 1
+        numbers_with_names << "#{placeholdercounter}. #{name}"
+      end
+    joined = numbers_with_names.join(" ")
+    puts "The line is currently: #{joined.to_s}"
+  end
+end
+
+def take_a_number(deli_queue,)
+end
