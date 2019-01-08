fd994cb7bb8d069f56e255e5f97512b918280dea
Toni-Ty <antonique2479@yahoo.com>
2019-01-08 06:59:01 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else
+    status = "The line is currently:"
+
+    katz_deli.each_with_index do |value, index| # returns the value or element itself (the person) while index returns their number in line, and appears to add one.
+      status += " #{index.to_i+1}. #{value}"
+    end
+      puts "#{status}"
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number#{katz_deli.length} in line."
