358846e0aa99d617849107c8493390c1b1e1ad55
Brandon Brasson <rocksong622@gmail.com>
2019-01-10 18:32:29 +0000

    Done.

--- spec/deli_counter_spec.rb
+++ spec/deli_counter_spec.rb
@@ -1,10 +1,10 @@
-puts describe 'Deli Counter' do
+ describe 'Deli Counter' do
 
   let(:katz_deli) { [] }
   let(:other_deli) { ["Logan", "Avi", "Spencer"] }
   let(:another_deli) { ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"] }
 
-  puts describe "#line" do
+ describe "#line" do
     context "there is nobody in line" do
       it "should say the line is empty" do
         # This line checks the current standard output (your terminal screen)
@@ -20,14 +20,14 @@ puts describe 'Deli Counter' do
         line(other_deli)
       end
 
-      puts it 'works with different people in line' do
+       it 'works with different people in line' do
 	      expect($stdout).to receive(:puts).with("The line is currently: 1. Amanda 2. Annette 3. Ruchi 4. Jason 5. Logan 6. Spencer 7. Avi 8. Joe 9. Rachel 10. Lindsey")
 	line(another_deli)
       end
     end
   end
 
-  puts describe "#take_a_number" do
+   describe "#take_a_number" do
     context "there is nobody in line" do
       it "should add a person to the line" do
         expect($stdout).to receive(:puts).with("Welcome, Ada. You are number 1 in line.")
