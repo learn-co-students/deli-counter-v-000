d6fd769895538b24309daf31a9980607eb18aed2
smithwebtek <brad@smithwebtek.com>
2019-01-25 17:06:13 -0500

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,33 @@
+require 'pry'
+
+
+#  NOTE you can << into a string, but you cannot .push into a string
+
 def line(katz_deli)
   customers = String.new
-  katz_deli.each_with_index { |item, index| customers << " #{index+1}. #{item}"}
+	katz_deli.each_with_index { |item, index| customers << " #{index+1}. #{item}"}
     if customers == ""
       puts "The line is currently empty."
-    else   
+		else
       puts "The line is currently:#{customers}"
   end
 end
 
+# def line(katz_deli)
+# 	if katz_deli.length == 0
+# 		puts "The line is currently empty."
+# 		return
+#   else 
+#     new_array = String.new
+#     katz_deli.each_with_index do |person, index|
+#       new_array << (" #{index+1}. #{person}")
+#     end
+#   end
+#   puts "The line is currently:" + "#{new_array}"
+# end
+
 def take_a_number(katz_deli, name)
-    customers = String.new
-    number = String.new
+    number = ""
   if katz_deli.count == 0
     katz_deli << name
     puts "Welcome, #{name}. You are number 1 in line."
